d462b4ad3543dbebae64e3b3184183fb1045849a
kiwilss <m15205622327@163.com>
2018-04-20 14:05:09 +0800

    4-20 demo enum

--- src/com/kiwi/kotlin/all_enum/Color2.kt
+++ src/com/kiwi/kotlin/all_enum/Color2.kt
@@ -11,6 +11,19 @@
 
 package com.kiwi.kotlin.all_enum
 
-enum class Color2(val r: Int,val g: Int,val b:Int) {//声明属性
+enum class Color2(val r: Int,val g: Int,val b:Int) {//声明枚举常量的属性
+    RED(255,0,0),ORANGE(255,165,0)
+    ,YELLOW(255,255,0),GREEN(0,255,0)
+    ,BLUE(0,0,255),INDIGO(75,0,130)
+    ,VIOLET(238,130,238);//必须加分号
 
+
+    //定义各个方法
+    fun rgb () :Int = (r*256+g)*256+b
+
+}
+
+fun main(args: Array<String>) {
+    println(Color2.RED.rgb())
+    println(Color2.GREEN)
 }
