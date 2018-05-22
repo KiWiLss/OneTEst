5523c1db941cceaec716e92670137db157fda48f
kiwilss <m15205622327@163.com>
2018-05-22 17:59:56 +0800

    5-22

--- src/com/kiwi/kotlin3/demo_opertor/Foo.kt
+++ src/com/kiwi/kotlin3/demo_opertor/Foo.kt
@@ -1,5 +1,10 @@
 package com.kiwi.kotlin3.demo_opertor
 
+import org.omg.PortableServer.portable.Delegate
+import java.lang.reflect.Type
+import kotlin.reflect.KProperty
+
+
 /**
  * Copyright (C), 2017-2018, XXX有限公司
  * FileName: Foo
@@ -10,3 +15,22 @@ package com.kiwi.kotlin3.demo_opertor
  * <author>          <time>          <version>          <desc>
  * 作者姓名           修改时间           版本号              描述
  */
+class Foo{
+
+    var p: Type by Delegate()
+
+
+
+}
+class Delegate{
+//    operator fun getValue(){
+//    }
+//    operator fun setValue(){}
+    operator fun getValue(foo: Foo, property: KProperty<*>): Type {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    operator fun setValue(foo: Foo, property: KProperty<*>, type: Type) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
