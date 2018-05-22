f4abba1b9c5e6756e22e508e1d8904d60c8c8957
kiwilss <m15205622327@163.com>
2018-05-22 17:54:12 +0800

    5-22

---
+++ src/com/kiwi/kotlin3/demo_opertor/Foo.kt
@@ -0,0 +1,12 @@
+package com.kiwi.kotlin3.demo_opertor
+
+/**
+ * Copyright (C), 2017-2018, XXX有限公司
+ * FileName: Foo
+ * Author:   kiwilss
+ * Date:     2018/5/22 17:53
+ * Description: foo
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
